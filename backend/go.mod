049fc3f6e803aa3b5e8fb912fe42209d889e0a06
khinthu01 <khinthu01@gmail.com>
2021-10-15 14:24:01 +1100

    connect to mongodb

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,13 @@ module github.com/khinthu01/Coding-test/backend
 go 1.17
 
 require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.7.3
+)
+
+require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -15,6 +19,4 @@ require (
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/text v0.3.5 // indirect
-    go.mongodb.org/mongo-driver v1.7.3
-    github.com/gorilla/mux v1.8.0
 )
--- backend/main.go
+++ backend/main.go
@@ -1,30 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"encoding/json"
+	"time"
 
 	"github.com/gorilla/mux"
+
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"Desc"`
-	Content string `json:"Content"`
+
+
+type Order struct {
+	OrderName string `json:"Order name"`
+	CustomerCompany string `json:"Customer Company"`
+	CustomerName string `json:"Customer name"`
+	OrderDate time.Time `json:"Order date"`
+	DeliveredAmount float64 `json:"Delivered Amount"`
+	TotalAmount float64 `json:"Total Amount"`
 }
 
-type Articles []Article
+type Orders []Orders
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title:"Test Title", Desc: "Test Description", Content: "Hello World",},
+func connectToDB() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+    	log.Fatal(err)
 	}
 
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
 
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+	if err != nil {
+    	log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
+
+func allOrders(w http.ResponseWriter, r *http.Request) {
+	
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -42,4 +67,5 @@ func handleRequests() {
 
 func main() {
 	handleRequests()
+	connectToDB()
 }
