392ce05e0bde47bb472dd549835a3fe8e9508d11
khinthu01 <khinthu01@gmail.com>
2021-10-15 13:36:15 +1100

    go.mod

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module go.mongodb.org/mongo-driver
+
+go 1.17
+
+require go.mongodb.org/mongo-driver v1.7.3
---
+++ backend/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+
+	
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/readpref"
+)
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title:"Test Title", Desc: "Test Description", Content: "Hello World",},
+	}
+
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Endpoint Hit")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
