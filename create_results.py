0e19b56bad07d30479b102875121f346419f54dd
khinthu01 <khinthu01@gmail.com>
2021-10-21 17:21:35 +1100

    create results table

--- create_results.py
+++ create_results.py
@@ -36,8 +36,22 @@ order_items_sql = """SELECT * from public.order_items"""
 db_cursor.execute(order_items_sql)
 order_items = db_cursor.fetchall()
 
-order_list = []
+#order_list = []
 
+create_table = (
+    """
+    CREATE TABLE orderlist (
+        order_item_id INTEGER PRIMARY KEY,
+        order_name VARCHAR(10),
+        customer_company VARCHAR(30),
+        customer_name VARCHAR(30),
+        order_date TIMESTAMP NOT NULL,
+        delivered_amount FLOAT(6),
+        total_amount FLOAT(6)
+    )
+    """)
+
+db_cursor.execute(create_table)
 
 for order in order_items:
     i = 0
@@ -62,33 +76,51 @@ for order in order_items:
     company_name = companies[i]['company_name']
 
     if order[3] == None or order[2] == None:
-        amount = '-'
+        amount = 0
     else:
         amount = int(order[3])*float(order[2])
 
-    list_item = {
-        "order_item_id": order[0],
-        "order_name": order_name,
-        "customer_company": company_name,
-        "customer_name": customer_name,
-        "order_date": order_date,
-        "delivered_amount": amount,
-        "total_amount": amount
-    }
+    # list_item = {
+    #     "order_item_id": order[0],
+    #     "order_name": order_name,
+    #     "customer_company": company_name,
+    #     "customer_name": customer_name,
+    #     "order_date": order_date,
+    #     "delivered_amount": amount,
+    #     "total_amount": amount
+    # }
 
-    order_list.append(list_item)
+    insert_command = (
+        """INSERT INTO orderlist(
+            order_item_id,
+            order_name,
+            customer_company,
+            customer_name,
+            order_date,
+            delivered_amount,
+            total_amount
+        ) VALUES (
+            %s, %s, %s, %s, %s, %s, %s
+        )
+        """)
 
-create_table = (
-    """
-    CREATE TABLE orderlist (
-        order_item_id INTEGER PRIMARY KEY,
-        order_name VARCHAR(10),
-        customer_company VARCHAR(30),
-        customer_name VARCHAR(30),
-        order_date TIMESTAMP NOT NULL,
-        delivered_amount FLOAT(6),
-        total_amount FLOAT(6)
+    values = (
+        order[0],
+        order_name,
+        company_name,
+        customer_name,
+        order_date,
+        amount,
+        amount
     )
-    """)
 
-db_cursor.execute(create_table)
+    db_cursor.execute(insert_command, values)
+
+
+db_cursor.close()
+
+connection.commit()
+connection.close()
+
+
+
